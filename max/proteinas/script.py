689c9f3b247760cf477d62d1544ccb28f8f59658
Bruno da Silva Castilho <bruno.silva.castilho@gmail.com>
2025-09-29 17:02:20 -0300

    refact: scraping max

--- max/proteinas/script.py
+++ max/proteinas/script.py
@@ -1,7 +1,8 @@
-
-import requests # para requisições http
-import json # para gerar JSON a partir de objetos do Python
-from bs4 import BeautifulSoup # BeautifulSoup é uma biblioteca Python de extração de dados de arquivos HTML e XML.
+import requests  # para requisições http
+import json  # para gerar JSON a partir de objetos do Python
+import re
+from bs4 import BeautifulSoup  # extrair dados de HTML
+import re
 
 
 requisicaoDePagina = requests.get('https://www.maxtitanium.com.br/produtos/proteinas')
@@ -28,58 +29,70 @@ produtos = site.find_all("article")
 resposta = []
 
 for produto in produtos:
+    if not 'product-card' in produto['class']:
+        continue
 
-    if not 'product-card' in produto['class']: continue
     audRelative = produto.find("div", {'class': 'aud-relative'})
     a = audRelative.find("a") if audRelative else None
-    link = a["href"]  if a else None
+    link = f'https://www.maxtitanium.com.br{a["href"]}' if a else None
     figura = a.find("source")["srcset"] if a else None
 
-    print(link)
-    print(figura)
-
     h3 = produto.find("h3", {'class': 'product-card__title'})
-    titulo =  h3.get_text(strip=True)
-
-    print(titulo)
-
+    titulo = h3.get_text(strip=True).replace('\xa0', ' ') if h3 else None
 
     spanPrecoAnterior = produto.find("span", {'class': 'product-card__pricefrom'})
     spanPrecoPrincipal = produto.find("span", {'class': 'product-card__priceper'})
 
-
-    precoAnterior = spanPrecoAnterior.get_text(strip=True) if spanPrecoAnterior else None
-    precoPrincipal = spanPrecoPrincipal.get_text(strip=True) if spanPrecoPrincipal else None 
-
-    print(precoAnterior)
-    print(precoPrincipal)
+    precoAnterior = spanPrecoAnterior.get_text(strip=True).replace('\xa0', ' ') if spanPrecoAnterior else None
+    precoPrincipal = spanPrecoPrincipal.get_text(strip=True).replace('\xa0', ' ') if spanPrecoPrincipal else None
 
     parcelamentoDiv = produto.find("div", {"class": 'product-card__installments'})
-    parcelamento = parcelamentoDiv.get_text(strip=True) if parcelamentoDiv else None 
-
-    print(parcelamento)
+    parcelamento = parcelamentoDiv.get_text(strip=True).replace('\xa0', ' ') if parcelamentoDiv else None
 
     precoPixDiv = produto.find("div", {"class": 'aud-font-semibold'})
-    precoPix = precoPixDiv.get_text(strip=True) if precoPixDiv else None 
-    print(precoPix)
+    precoPix = precoPixDiv.get_text(strip=True).replace('\xa0', ' ') if precoPixDiv else None
+
+    offSpan = produto.find('span', {'class': 'tags-render__text'})
+    off = offSpan.get_text() if offSpan else None
+
+    responseProduct = requests.get(link)
+    productContent = responseProduct.content
+
+    soup = BeautifulSoup(productContent, 'html.parser')
+
+    ref = soup.find('div', id=re.compile(r'-product-reference')).get_text()
+
+    specifications = soup.find('div', id=re.compile(r'product-specifications'))
+    descriptionDiv = specifications.find('div', {'class': 'content'})
+
+    description = descriptionDiv.get_text() if descriptionDiv else ''
 
 
+    div = soup.find('div', id='slot-5a3a63c9-a77f-4b0f-ae5e-797f77ac16ba')
+    spans = div.find_all('span')
+
+    benefits = []
+    for span in spans:
+        benefits.append(span.get_text())
 
     dados = {
-        'TITULO': titulo, 
+        'TITULO': titulo,
         'LINK': link,
         'FIGURA': figura,
         'PRECO_ANTERIOR': precoAnterior,
         'PRECO_PRINCIPAL': precoPrincipal,
         'PARCELAMENTO': parcelamento,
-        'PRECO_PIX': precoPix
+        'PRECO_PIX': precoPix,
+        'OFF': off,
+        'REF': ref,
+        'DESCRICAO': description,
+        'BENEFICIOS': benefits,
     }
 
     resposta.append(dados)
-    #um print para separar os produtos
-    print("....")
 
-# Converte os objetos Pyhton em objeto JSON e exporta para o noticias.json
-with open('produtos.json', 'w') as arquivo:
-  arquivo.write(str(json.dumps(resposta, indent=7)))
+
+with open('produtos.json', 'w', encoding='utf-8') as arquivo:
+    json.dump(resposta, arquivo, indent=4, ensure_ascii=False)
+
 print("Created Json File")
